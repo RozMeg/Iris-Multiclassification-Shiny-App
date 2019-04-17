0f9bfb3782c6550e8f55a535e139bf2279b8a391
RozMeg <35344848+RozMeg@users.noreply.github.com>
2019-04-17 17:47:44 -0600

    Final updates before submission

--- global.R
+++ global.R
@@ -6,31 +6,33 @@
 library(tidyverse)
 library(xgboost)
 
-#At first, I thought that the prompt meant we had to use the table and get the probabilities based on the predictions we got when we
-#ran xgboost on the whole thing. I didn't realize we were supposed to just run the xgboost model on the new inputs. I commented out
-#the previous code and added the correct code above.
 
 #load saved xgboost model
 xgmodel <- xgb.load("IrisClassifier.model")
+
+#I'm not sure what's going on with the order and labels in preds. I can see that new predictions are generated, but I don't
+#think they're being made in setosa, versicolor, virginica order, which is what I had thought initially
+#You'll see in the app that my graphs update based on input, but unfortunately, the table doesn't reorder species accordingly.
 
 #create function to generate prediction table
 pred_gen <- function(slength, swidth, plength, pwidth){
   #make prediction based on inputs
   preds <- predict(IrisClassifier, as.matrix(slength,swidth,plength,pwidth))
-  
-  #create data frame with probabilities
-  #cbind and colnames threw errors when I wanted to chain, hence, the ugly code
-  #had to throw in another column with the names since the labels wouldn't cooperate
-  t <- as.data.frame(preds)%>%
-    cbind(c("setosa","versicolor","virginica"))
-  colnames(t) = c("probability","species")
-  #sort by descending probability
-  ts<- t%>%
-      arrange(desc(probability))
-    #return dataframe
-    return(ts)
-  
+  # create data frame with probabilities
+  # cbind and colnames threw errors when I wanted to chain, hence, the ugly code
+  # had to throw in another column with the names since the labels wouldn't cooperate
+  t <- as.data.frame(preds)
+  #assuming these are in 1,2,3 label order? but that doesn't seem to be the case
+  rownames(t) = c("setosa","versicolor","virginica")
+  #give a label name so we can manipulate
+  colnames(t) = c("probabilities")
+  #arrange based on descending probabilities
+  t%>%arrange(desc(probabilities))
+  return(t)
 }
+
+#At first, I thought that the prompt meant we had to use the table and get the probabilities based on the predictions we got when we
+#ran xgboost on the whole thing, which doesn't make any sense
 
 # #setting up the dataset again
 # #get iris species as y variable
@@ -79,6 +81,6 @@ pred_gen <- function(slength, swidth, plength, pwidth){
 #   return(ts)
 # }
 
-#Test
-k <- pred_gen(7.0, 3.2,4.7,1.4)
-k
+# #Test
+# d <- pred_gen(7,3,4,0.2)
+# d
