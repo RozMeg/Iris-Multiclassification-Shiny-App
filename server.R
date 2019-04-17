d36a0fd3999e8c7536c686dae9aec39609aa8239
RozMeg <35344848+RozMeg@users.noreply.github.com>
2019-04-17 15:01:57 -0600

    Updated server and ui

---
+++ server.R
@@ -0,0 +1,55 @@
+#Server for iris multiclassification
+library(DT)
+library(tidyverse)
+library(ggplot2)
+
+#Define server logic
+server <- function(input, output){
+  #reactive data set that creates dataframe of species prediction probabilities in descending order
+  pred_probs <- reactive({
+    pred_gen(input$slength, input$swidth, input$plength, input$pwidth)
+  })
+  
+  #create output table for prediction probabilities
+  output$probTable <- DT::renderDataTable({pred_probs()})
+  
+  #create Scatterplot with Sepal Length and Sepal Width. Color based on the Species 
+  #and add a red point on the plot that shows where the values are set for prediction
+  output$scatter <- renderPlot({p <-
+                                 ggplot(data = main, aes(x=main$Sepal.Width,y=main$Sepal.Length))+
+                                 geom_point(aes(color=Species, shape=Species)) + labs(x="Sepal Width",y="Sepal Length")+
+                                 geom_point(aes(x=input$swidth, y=input$slength), colour="red")
+                               print(p)})
+  
+  #create density plots for Sepal.Length, Sepal.Width, Petal.Length, and Petal.Width that have a vertical line 
+  #demonstrating where the sliders are set. For these plots, set the fill to Species to add additional context
+  
+  #density plot for sepal width
+  output$swdensity <- renderPlot({sw <- 
+                                   ggplot(data=main, aes(x=main$Sepal.Width, fill=main$Species))+
+                                   geom_vline(aes(xintercept = input$Sepal.Width), color = "red", linetype = "dashed")
+  print(sw)})
+  
+  #density plot for sepal length
+  output$sldensity <- renderPlot({sl <- 
+    ggplot(data=main, aes(x=main$Sepal.Length, fill=main$Species))+
+    geom_vline(aes(xintercept = input$Sepal.Length), color = "red", linetype = "dashed")
+  print(sl)})
+  
+  #density plot for petal width
+  output$pwdensity <- renderPlot({pw <- 
+    ggplot(data=main, aes(x=main$Petal.Width, fill=main$Species))+
+    geom_vline(aes(xintercept = input$Petal.Width), color = "red", linetype = "dashed")
+  print(pw)})
+  
+  #density plot for petal width
+  output$pldensity <- renderPlot({pl <- 
+    ggplot(data=main, aes(x=main$Petal.Length, fill=main$Species))+
+    geom_vline(aes(xintercept = input$Petal.Length), color = "red", linetype = "dashed")
+  print(pl)})
+}
+
+  
+  
+  
+  
